63023ef47e12404cc9828c6c149e3c8d63f843e1
Kwabena Osei <kwabosei000gmail.com>
2026-02-17 02:20:47 +0000

    My work and understanding of the introduction to Python

---
+++ hiWorld.py
@@ -0,0 +1,24 @@
+# This isn't my first HelloWorld unfortunately :). Nonetheless I'm always ready to learn from scratch
+# Introduction
+
+# And it started with the print() function which basically outputs data and displays it onto the console. At least that's my understanding of it.
+print("Hi World! Just decided to change things this time")
+
+# The next part is using math symbols to perform various calculations in Python. And they help a lot, funny enough, when coding in Python.
+print(13 + 25)   # addition(+)
+print(2 - 16)   # subtraction(-). And yes, it shows the negative sign.
+print(2 * 3)   # multiplication(*)
+print(3 / 2)   # division(/)
+print(3 ** 2)  # exponential(**). The first number before the sign is the base and the other number (that is after the sign) is the power.
+print(3 % 2)   # modulus(%). This just outputs the remainder of a division and the position of the numbers matter unlike multiplication.
+print(10 // 3)  # Floor division operator(//). And this shows how many times the denominator appears in the numerator. For this example, there are 3 3s in 10.
+
+# And the last part is about the data types in Python
+print(type(10))                  # Integer
+print(type(4.63))                # Float
+print(type(2 + 3j))              # Complex. Even I didn't know complex numbers also existed here
+print(type('Kwabena'))          # String
+print(type([2, 3, 5]))           # List
+print(type({'name': 'Kwabena'}))  # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set.
+print(type(('item1', 'item2','item3'))) # Tuple.
