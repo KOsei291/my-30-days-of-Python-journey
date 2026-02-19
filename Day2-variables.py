749ea98a5956ffbffda033ff15797eb3f8186e78
Kwabena Osei <kwabosei000gmail.com>
2026-02-19 01:24:51 +0000

    My work and understanding of the introduction to Python

---
+++ Day2-variables.py
@@ -0,0 +1,22 @@
+# Today is about variables. an example of one is this below
+name = 'Kwabena' # For this, the value of the variable is a string. You can store all data types into variables
+country = 'Ghana'
+age = 250 # This is an integer. Floats can also be stored. Either add a decimal point, or declare it a float by using float(variable_name). variable_name is whatever variable you create.
+is_married = False # This is for boolean
+skills = ['HTML', 'Linux', 'Python'] # Lists too can be stored in a variable
+
+# There are functions you can use to change the state of the value of the variable. For example int(), float(), and str()
+# You can also ask the user to put in a value for your variable using input()
+# Other functions to help you when coding are min() which prints out the minimum value (works on numbers and a list of numbers), max() shows the max, len() prints the length of the value
+# type() shows data type, list() creates a list and dict() creates a dictionary.
+
+person_info = {
+    'name': 'Kwabena',
+    'age': 250,
+    'country': 'Ghana',
+    'city': 'Accra'
+}
+
+# You can also create variables in one line. Use commas to separate the variable names and the values.
+# An example is this:
+# name, country, age, is_married = 'Kwabena', 'Ghana', 250, False
